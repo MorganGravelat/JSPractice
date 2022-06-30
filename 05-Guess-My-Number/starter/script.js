0e6c94dc0cd2d6bc9167c3b077ece1a2dc8918aa
MorganGravelat <mgravelat@gmail.com>
2022-06-30 13:36:37 -0400

    A bit more done

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -2,10 +2,10 @@
 let number = 0;
 let score = 20;
 let hiScore = 0;
-let win = false;
+let win = 0;
 
 function decideNumber() {
-    number = Math.floor((Math.random() * 100) + 1)
+    number = Math.trunc(Math.random() * 20) + 1;
     document.getElementById('guessText').innerHTML = "Start guessing..."
     console.log(number);
     score = 20;
@@ -13,22 +13,32 @@ function decideNumber() {
 }
 
 function checkGuess() {
+    console.log(win);
     if (win) {
-        document.getElementById('guessText').innerHTML = "I said press the again BUTTON!"
+        console.log('this is happening')
+        if (win > 1) {
+            document.getElementById('guessText').innerHTML = "You cannot be serious! Fine just sit here and wait..."
+        } else {
+            document.getElementById('guessText').innerHTML = "I said press the again BUTTON!"
+        }
+        win++;
     }
     let guess = document.getElementById('guessNumber').value;
-    if (guess == number) {
+    if (Number(guess) === number && !win) {
         document.getElementById('guessText').innerHTML = "You Win! Press The Again Button In The Top left corner!"
         win = true;
         if (score > hiScore) hiScore = score;
 
+    }
+    else if (guess > 20 || guess < 1) {
+        document.getElementById('guessText').innerHTML = "The number must be between 1 and 20"
     }
     else if (guess < number) {
         document.getElementById('guessText').innerHTML = "Too Low"
         score--;
         document.getElementById('scoreLabel').innerHTML = score
     }
-    else {
+    else if (guess > number) {
         document.getElementById('guessText').innerHTML = "Too High"
     }
 
